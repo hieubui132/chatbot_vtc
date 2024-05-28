c2677fab71209792e1860eb2616853b3a5390a57
Bùi Hiếu <buichihieu1321999@gmail.com>
2024-05-28 22:23:34 +0700

    test

--- src/modules/chat/Chat.service.ts
+++ src/modules/chat/Chat.service.ts
@@ -8,22 +8,22 @@ export class ChatService {
   constructor(private readonly httpService: HttpService) {}
 
   getHello(): string {
-    return 'Hello Chatbot By ' + process.env.NAME;
+    return 'Hello Chatbot By Hieubc';
   }
 
-  async findAll() {
-    const { data } = await firstValueFrom(
-      this.httpService
-        .get(
-          'https://graph.facebook.com/v20.0/327329587129436/conversations?platform=messenger&access_token=EAAN7jP8sNV8BO7zthJuN4YeNEa44Eis9kihvrNT9xHMLw1Dk5KZCZAGKU28KE90Wh9bLf6p0J1hq5Vspli6NhCioAzX7RzMgrg3FC3tUoXc8CvhehDmkPPgerRMLeQjh26526V9TeIPHdwhghKmts2qt0yiJNp5cS2Et5fDuaSG9iC0ZAEad0H9h8MNy0sbDwZDZD',
-        )
-        .pipe(
-          catchError((error: AxiosError) => {
-            console.log(error.response.data);
-            throw 'An error happened!';
-          }),
-        ),
-    );
-    return data;
-  }
+  // async findAll() {
+  //   const { data } = await firstValueFrom(
+  //     this.httpService
+  //       .get(
+  //         'https://graph.facebook.com/v20.0/327329587129436/conversations?platform=messenger&access_token=EAAN7jP8sNV8BO7zthJuN4YeNEa44Eis9kihvrNT9xHMLw1Dk5KZCZAGKU28KE90Wh9bLf6p0J1hq5Vspli6NhCioAzX7RzMgrg3FC3tUoXc8CvhehDmkPPgerRMLeQjh26526V9TeIPHdwhghKmts2qt0yiJNp5cS2Et5fDuaSG9iC0ZAEad0H9h8MNy0sbDwZDZD',
+  //       )
+  //       .pipe(
+  //         catchError((error: AxiosError) => {
+  //           console.log(error.response.data);
+  //           throw 'An error happened!';
+  //         }),
+  //       ),
+  //   );
+  //   return data;
+  // }
 }
