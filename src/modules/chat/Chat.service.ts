9195a50b1b24983a60b91ea56c48f0c600f2da5d
Bùi Hiếu <buichihieu1321999@gmail.com>
2024-06-03 20:21:33 +0700

    test

--- src/modules/chat/Chat.service.ts
+++ src/modules/chat/Chat.service.ts
@@ -2,21 +2,14 @@ import { Injectable } from '@nestjs/common';
 import { HttpService } from '@nestjs/axios';
 import { catchError, firstValueFrom } from 'rxjs';
 import { AxiosError } from 'axios';
+import { Cron, CronExpression } from '@nestjs/schedule';
 
 @Injectable()
 export class ChatService {
   constructor(private readonly httpService: HttpService) {}
 
-  // onModuleInit() {
-  //   this.startInterval();
-  // }
-
-  // private startInterval() {
-  //   setInterval(() => {
-  //     this.getPSID();
-  //   }, 20000); // 20000ms = 20s
-  // }
-  // async getPSID() {
-  //   console.log('count');
-  // }
+  @Cron(CronExpression.EVERY_30_SECONDS)
+  handleCron() {
+    console.log('Called every 30 seconds');
+  }
 }
