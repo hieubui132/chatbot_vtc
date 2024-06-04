567941194e7cd837b72dac33fca74557c8d4d029
Bùi Hiếu <buichihieu1321999@gmail.com>
2024-06-04 07:17:29 +0700

    test

--- src/modules/chat/Chat.service.ts
+++ src/modules/chat/Chat.service.ts
@@ -8,20 +8,20 @@ import { Cron, CronExpression } from '@nestjs/schedule';
 export class ChatService {
   constructor(private readonly httpService: HttpService) {}
 
-  @Cron(CronExpression.EVERY_30_SECONDS)
-  async getMessage() {
-    const { data } = await firstValueFrom(
-      this.httpService
-        .get(
-          'https://graph.facebook.com/v20.0/327329587129436/conversations?platform=messenger&access_token=EAAN7jP8sNV8BO7zthJuN4YeNEa44Eis9kihvrNT9xHMLw1Dk5KZCZAGKU28KE90Wh9bLf6p0J1hq5Vspli6NhCioAzX7RzMgrg3FC3tUoXc8CvhehDmkPPgerRMLeQjh26526V9TeIPHdwhghKmts2qt0yiJNp5cS2Et5fDuaSG9iC0ZAEad0H9h8MNy0sbDwZDZD',
-        )
-        .pipe(
-          catchError((error: AxiosError) => {
-            throw 'An error happened!';
-          }),
-        ),
-    );
-    console.log(data);
-    return data;
-  }
+  // @Cron(CronExpression.EVERY_30_SECONDS)
+  // async getMessage() {
+  //   const { data } = await firstValueFrom(
+  //     this.httpService
+  //       .get(
+  //         'https://graph.facebook.com/v20.0/327329587129436/conversations?platform=messenger&access_token=EAAN7jP8sNV8BO7zthJuN4YeNEa44Eis9kihvrNT9xHMLw1Dk5KZCZAGKU28KE90Wh9bLf6p0J1hq5Vspli6NhCioAzX7RzMgrg3FC3tUoXc8CvhehDmkPPgerRMLeQjh26526V9TeIPHdwhghKmts2qt0yiJNp5cS2Et5fDuaSG9iC0ZAEad0H9h8MNy0sbDwZDZD',
+  //       )
+  //       .pipe(
+  //         catchError((error: AxiosError) => {
+  //           throw 'An error happened!';
+  //         }),
+  //       ),
+  //   );
+  //   console.log(data);
+  //   return data;
+  // }
 }
