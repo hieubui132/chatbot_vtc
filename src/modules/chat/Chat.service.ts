2905059b505f3e2d467bd44055ee5c3756fe383d
Bùi Hiếu <buichihieu1321999@gmail.com>
2024-06-10 04:59:40 +0700

    test

--- src/modules/chat/Chat.service.ts
+++ src/modules/chat/Chat.service.ts
@@ -7,27 +7,4 @@ import { Cron, CronExpression } from '@nestjs/schedule';
 @Injectable()
 export class ChatService {
   constructor(private readonly httpService: HttpService) {}
-
-  @Cron(CronExpression.EVERY_30_SECONDS)
-  async getMessage() {
-    const { data } = await firstValueFrom(
-      this.httpService
-        .get(
-          `https://graph.facebook.com/v20.0/${process.env.PAGE_ID}/conversations?platform=messenger&access_token=${process.env.PAGE_ACCESS_TOKEN}`,
-        )
-        .pipe(
-          catchError((error: AxiosError) => {
-            throw 'An error happened!';
-          }),
-        ),
-    );
-    this.processData(data);
-  }
-
-  // Hàm xử lý dữ liệu
-  async processData(data: any) {
-    // Thực hiện logic của bạn tại đây
-    console.log(data); // ví dụ, in dữ liệu ra console
-    // Hãy thay thế phần trên bằng mã của bạn để xử lý dữ liệu
-  }
 }
